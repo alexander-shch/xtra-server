5cbe3defbeec1b90b9e1aff46df629d566883704
Alexander Shcherbakov <alexander.sh.sa@gmail.com>
2020-09-08 22:41:27 +0200

    new categories end points

--- src/routing.ts
+++ src/routing.ts
@@ -5,6 +5,7 @@ import userRouter from './api/user';
 import permissionsRouter from './api/permissions';
 import buildingsRouter from './api/buildings';
 import classesRouter from './api/classes';
+import categoriesRouter from './api/categories';
 
 const appRouter = Router();
 
@@ -13,5 +14,6 @@ appRouter.use('/user', userRouter);
 appRouter.use('/permissions', permissionsRouter);
 appRouter.use('/buildings', buildingsRouter);
 appRouter.use('/classes', classesRouter);
+appRouter.use('/categories', categoriesRouter);
 
 export default appRouter;
