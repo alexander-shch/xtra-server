6206d5f345ba40a5fe2d147c4a231554abe3c7d5
Alexander Shcherbakov <alexander.sh.sa@gmail.com>
2020-09-15 12:59:56 +0200

    remove dist

--- dist/settings/permissions.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DefaultPermissionsSuperUser = exports.DefaultPermissions = void 0;
-var helper_1 = require("./helper");
-exports.DefaultPermissions = helper_1.createPermissionsList(false);
-exports.DefaultPermissionsSuperUser = helper_1.createPermissionsList(true);
