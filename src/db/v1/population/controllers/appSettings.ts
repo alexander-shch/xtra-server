da8eddd8086ba7944f790444ed0a6587dc4affc3
Alexander Shcherbakov <alexander.sh.sa@gmail.com>
2020-10-13 11:49:58 +0200

    add population for settings in case they do not exist

---
+++ src/db/v1/population/controllers/appSettings.ts
@@ -0,0 +1,37 @@
+import SettingsDefaults from '../../../../settings/appSettings';
+import {
+  createSettingMany,
+  findSettings,
+} from '../../models/settings/controller';
+
+export async function populateAppSettings(): Promise<void> {
+  const settingsList = Object.values(SettingsDefaults);
+  const settingNameList = settingsList.map(({ name }) => name);
+  const queryResults = await findSettings({ name: { $in: settingNameList } });
+  // If no setting were found we populate them all
+  if (queryResults.length === 0) {
+    console.log(
+      `Populated ${queryResults.length} new settings "${queryResults
+        .map(({ name }) => name)
+        .join()}"`
+    );
+    return createSettingMany(settingsList).then(() => void 0);
+  }
+  const needToBePopulated = settingsList.filter(
+    ({ name: needToBePopulatedName }) =>
+      queryResults.every(({ name }) => name !== needToBePopulatedName)
+  );
+  // If some setting were found we filter out the existing and populate the rest
+  if (needToBePopulated.length > 0) {
+    console.log(
+      `Populated ${
+        needToBePopulated.length
+      } new settings "${needToBePopulated.map(({ name }) => name).join()}"`
+    );
+
+    return createSettingMany(needToBePopulated).then(() => void 0);
+  }
+
+  console.log(`No settings were populated`);
+  return Promise.resolve();
+}
--- src/db/v1/population/index.ts
+++ src/db/v1/population/index.ts
@@ -1,6 +1,7 @@
 import { populateAdmin } from './controllers/admin';
-
+import { populateAppSettings } from './controllers/appSettings';
 
 export default async function populate() {
   await populateAdmin();
+  await populateAppSettings();
 }
