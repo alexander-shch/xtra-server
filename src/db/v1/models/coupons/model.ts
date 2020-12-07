bb292dfdd6e6330af77974433472205134122108
Alexander Shcherbakov <alexander.sh.sa@gmail.com>
2020-12-07 10:47:42 +0100

    update coupon model

--- src/db/v1/models/coupons/model.ts
+++ src/db/v1/models/coupons/model.ts
@@ -4,6 +4,9 @@ export interface ICoupon {
   code: string;
   isPercent: boolean;
   discount: number;
+  active: boolean;
+  effectiveFrom: Date;
+  effectiveTo: Date;
 }
 
 export type ICouponDOC = ICoupon & Document;
@@ -12,6 +15,9 @@ const CouponSchema = new Schema({
   code: { type: String, required: true, unique: true },
   isPercent: { type: Boolean, required: true },
   discount: { type: Number, required: true },
+  active: { type: Boolean, required: true },
+  effectiveFrom: { type: Date, required: false },
+  effectiveTo: { type: Date, required: false },
 });
 
 export default model<ICouponDOC>('coupons', CouponSchema);
